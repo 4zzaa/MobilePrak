911a61747975180af1f1e723959744292d4199dc
KGazz <aziz.hilmi1234@gmail.com>
2026-09-21 19:34:52 +0700

    Add modul 4

---
+++ modul 4/lib/main.dart
@@ -0,0 +1,111 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MusicPlayerApp());
+}
+
+class MusicPlayerApp extends StatelessWidget {
+  const MusicPlayerApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        brightness: Brightness.dark,
+        scaffoldBackgroundColor: const Color(0xFF212121),
+      ),
+      home: const MusicPlayerPage(),
+    );
+  }
+}
+
+class MusicPlayerPage extends StatelessWidget {
+  const MusicPlayerPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: const Color(0xFF212121),
+
+      body: SafeArea(
+        child: Column(
+          children: [
+            // Bagian tengah
+            Expanded(
+              child: Center(
+                child: Text(
+                  'Pemutar Musik',
+                  style: TextStyle(
+                    color: Colors.white,
+                    fontSize: 14,
+                  ),
+                ),
+              ),
+            ),
+            Container(
+              height: 70,
+              width: double.infinity,
+              color: const Color(0xFF181818),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                children: [
+                  // Shuffle
+                  IconButton(
+                    onPressed: () {},
+                    icon: const Icon(
+                      Icons.shuffle,
+                      color: Colors.white,
+                      size: 18,
+                    ),
+                  ),
+                  // Previous
+                  IconButton(
+                    onPressed: () {},
+                    icon: const Icon(
+                      Icons.skip_previous,
+                      color: Colors.white,
+                      size: 20,
+                    ),
+                  ),
+                  Container(
+                    width: 40,
+                    height: 40,
+                    decoration: const BoxDecoration(
+                      color: Colors.white,
+                      shape: BoxShape.circle,
+                    ),
+                    child: IconButton(
+                      padding: EdgeInsets.zero,
+                      onPressed: () {},
+                      icon: const Icon(
+                        Icons.play_arrow,
+                        color: Colors.black,
+                        size: 25,
+                      ),
+                    ),
+                  ),
+                  IconButton(
+                    onPressed: () {},
+                    icon: const Icon(
+                      Icons.skip_next,
+                      color: Colors.white,
+                      size: 20,
+                    ),
+                  ),                    IconButton(
+                    onPressed: () {},
+                    icon: const Icon(
+                      Icons.repeat,
+                      color: Colors.white,
+                      size: 18,
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
